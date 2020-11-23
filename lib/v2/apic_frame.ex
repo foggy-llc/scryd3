97adb44f24710f8e3b3a6d1c026bb1d6d7f0a4fc
Austin Lanari <austin@jumanji.io>
2020-11-22 16:07:10 -0800

    build frame when value is map; handle apic frames with missing encoding

--- lib/v2/apic_frame.ex
+++ lib/v2/apic_frame.ex
@@ -17,7 +17,8 @@ defmodule ScryD3.V2.ApicFrame do
     0x06 => "Media (e.g. lable side of CD)",
     0x07 => "Lead artist/lead performer/soloist",
     0x08 => "Artist/performer",
-    0x09 => "Conductor",    "0x0A" => "Band/Orchestra",
+    0x09 => "Conductor",
+    0x0A => "Band/Orchestra",
     0x0B => "Composer",
     0x0C => "Lyricist/text writer",
     0x0D => "Recording Location",
@@ -30,9 +31,16 @@ defmodule ScryD3.V2.ApicFrame do
     0x14 => "Publisher/Studio logotype"
   }
 
+  # Some things will be missing encoding (sonic test case again).
+  def read(<<encoding::integer-8, payload::binary>>) when encoding > 2 do
+    read(<<0>> <> payload)
+  end
+
   def read(<<encoding::binary-size(1), payload::binary>>) do
     {mime, rest, _} = ScryD3.V2.extract_null_terminated(encoding <> payload)
     <<type_code, 0, picture_data::binary>> = rest
+    picture_type = @picture_types[type_code]
+
     {description, picture_data, _} =
       try do
         ScryD3.V2.extract_null_terminated(encoding <> picture_data)
@@ -41,14 +49,14 @@ defmodule ScryD3.V2.ApicFrame do
           Logger.debug("Image lacking description! #{inspect(e)}")
           {nil, picture_data, nil}
       end
-    
-    %{
-      mime_type: mime,
-      picture_type: @picture_types[type_code],
-      description: description,
-      picture_data: picture_data,
-      text_encoding: encoding
-    }
+
+    {picture_type,
+     %{
+       mime_type: mime,
+       picture_type: picture_type,
+       description: description,
+       picture_data: picture_data,
+       text_encoding: encoding
+     }}
   end
-  
 end
